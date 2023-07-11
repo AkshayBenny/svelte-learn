037929c822b0458ec67aca12ba02c43b1cd48255
Akshay Benny <akshaybennyajh@gmail.com>
2023-07-11 16:50:13 +0530

    basic setup for header

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'DevBlog'
+export const description = 'A fast blog site made using SvelteKit for developers.'
+export const url = dev ? 'http://localhost:5173' : 'https://devblog.com'
---
+++ src/routes/header.svelte
@@ -0,0 +1,13 @@
+<script>
+	import * as config from '$lib/config'
+</script>
+
+<nav>
+	<a href="/">{config.title}</a>
+
+	<ul id="nav-links">
+		<li><a href="/about">About</a></li>
+		<li><a href="/contact">Contact</a></li>
+		<li><a href="/rss.xml" target="_blank">RSS</a></li>
+	</ul>
+</nav>
